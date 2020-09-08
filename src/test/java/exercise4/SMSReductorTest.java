c5c01a0dc471cb2d77145b8fe7810527efdcbb53
Sebastian <Sebastian@kursGIT.pl>
2020-09-08 23:20:57 +0200

    Commit exercise 3

--- src/test/java/exercise4/SMSReductorTest.java
+++ src/test/java/exercise4/SMSReductorTest.java
@@ -15,6 +15,19 @@ class SMSReductorTest {
         Assertions.assertEquals("DzisiajJestSobota,ImieninyKota", result);
 
     }
+    @Test
+    void shouldCountSMS() {
+        //given
+        String input = "    Podstawowym elementem wzorca jest interfejs Command, deklarujący metodę execute(). Jest to polimorficzna metoda reprezentująca polecenie do wykonania. Metoda ta jest implementowana w klasach ConcreteCommand w postaci polecenia wykonania określonej akcji na obiekcie-przedmiocie Receiver. Klient nie jest bezpośrednio związany ani z obiektem Command, ani z obiektem inicjującym jego wywołanie, czyli Invoker. Widzi jedynie odbiorcę wyników operacji ? obiekt Receiver.";
+        SMSReductor smsReductor = new SMSReductor();
+        //when
+        String result = smsReductor.smsReductionWithoutScanner(input);
+        int resultInt = smsReductor.amountOfSMS(result);
+        //then
+        Assertions.assertEquals("PodstawowymElementemWzorcaJestInterfejsCommand,DeklarującyMetodęExecute().JestToPolimorficznaMetodaReprezentującaPolecenieDoWykonania.MetodaTaJestImplementowanaWKlasachConcreteCommandWPostaciPoleceniaWykonaniaOkreślonejAkcjiNaObiekcie-przedmiocieReceiver.KlientNieJestBezpośrednioZwiązanyAniZObiektemCommand,AniZObiektemInicjującymJegoWywołanie,CzyliInvoker.WidziJedynieOdbiorcęWynikówOperacji?ObiektReceiver.", result);
+        Assertions.assertEquals(3, resultInt);
+
+    }
 
     @Test
     void shouldReduceSMSEmptyMessage() {
