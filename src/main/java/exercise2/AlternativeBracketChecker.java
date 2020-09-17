5994e4109725f5da6c3cc8d14ff80a60ca781e48
Sebastian <Sebastian@kursGIT.pl>
2020-09-17 13:51:26 +0200

    Exercise 2 commit with bracket formatter

--- src/main/java/exercise2/AlternativeBracketChecker.java
+++ src/main/java/exercise2/AlternativeBracketChecker.java
@@ -1,6 +1,9 @@
 package exercise2;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AlternativeBracketChecker {
     public boolean alternativeChecker(List<Bracket> listOfBrackets) {
@@ -37,6 +40,26 @@ public class AlternativeBracketChecker {
             }
         }
         return true;
+    }
+
+    public String bracketFormatter(String input) {
+        input = input.replaceAll("\n", "");
+        List<String> splittedInput = Arrays.asList(input.split("(?=[\\[{()}\\]])|(?<=[\\[{()}\\]])"));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int n = 0;
+        for (String string : splittedInput) {
+            if (string.startsWith("]") || string.startsWith("}") || string.startsWith(")")) {
+                n--;
+            }
+            stringBuilder.append("\t".repeat(n));
+            stringBuilder.append(string);
+            stringBuilder.append("\n");
+            if (string.startsWith("[") || string.startsWith("{") || string.startsWith("(") ) {
+                n++;
+            }
+        }
+        return stringBuilder.toString();
     }
 }
 
